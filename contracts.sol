4ddc93cbbdf8b307f62d4d9874b6fe0c266e01d5
Dexaran <dexaran820@gmail.com>
2023-09-14 02:02:30 +0400

    Update contracts.sol

--- contracts.sol
+++ contracts.sol
@@ -1,1 +1,247 @@
-1
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2;
+
+import "https://github.com/Dexaran/ERC223-token-standard/blob/development/token/ERC223/IERC223.sol";
+import "https://github.com/Dexaran/ERC223-token-standard/blob/development/token/ERC223/IERC223Recipient.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+library Address {
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * This test is non-exhaustive, and there may be false-negatives: during the
+     * execution of a contract's constructor, its address will be reported as
+     * not containing a contract.
+     *
+     * > It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     */
+    function isContract(address account) internal view returns (bool) {
+        // This method relies in extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+}
+
+/**
+ * @title Reference implementation of the ERC223 standard token.
+ */
+contract D223Token {
+
+     /**
+     * @dev Event that is fired on successful transfer.
+     */
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
+
+    string  private _name;
+    string  private _symbol;
+    uint8   private _decimals;
+    uint256 private _totalSupply;
+    
+    mapping(address => uint256) private balances; // List of user balances.
+
+    /**
+     * @dev Sets the values for {name} and {symbol}, initializes {decimals} with
+     * a default value of 18.
+     *
+     * To select a different value for {decimals}, use {_setupDecimals}.
+     *
+     * All three of these values are immutable: they can only be set once during
+     * construction.
+     */
+     
+    constructor()
+    {
+        _name     = "D223 presale token";
+        _symbol   = "pD223";
+        _decimals = 18;
+        balances[msg.sender] = 80000000 * 1e18;
+        _totalSupply = 80000000 * 1e18;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory)
+    {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view returns (string memory)
+    {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC223} uses, unless {_setupDecimals} is
+     * called.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC223-balanceOf} and {IERC223-transfer}.
+     */
+    function decimals() public view returns (uint8)
+    {
+        return _decimals;
+    }
+
+    /**
+     * @dev See {IERC223-totalSupply}.
+     */
+    function totalSupply() public view returns (uint256)
+    {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC223-standard}.
+     */
+    function standard() public view returns (string memory)
+    {
+        return "223";
+    }
+
+    
+    /**
+     * @dev Returns balance of the `_owner`.
+     *
+     * @param _owner   The address whose balance will be returned.
+     * @return balance Balance of the `_owner`.
+     */
+    function balanceOf(address _owner) public view returns (uint256)
+    {
+        return balances[_owner];
+    }
+    
+    /**
+     * @dev Transfer the specified amount of tokens to the specified address.
+     *      Invokes the `tokenFallback` function if the recipient is a contract.
+     *      The token transfer fails if the recipient is a contract
+     *      but does not implement the `tokenFallback` function
+     *      or the fallback function to receive funds.
+     *
+     * @param _to    Receiver address.
+     * @param _value Amount of tokens that will be transferred.
+     * @param _data  Transaction metadata.
+     */
+    function transfer(address _to, uint _value, bytes calldata _data) public returns (bool success)
+    {
+        // Standard function transfer similar to ERC20 transfer with no _data .
+        // Added due to backwards compatibility reasons .
+        balances[msg.sender] = balances[msg.sender] - _value;
+        balances[_to] = balances[_to] + _value;
+        if(Address.isContract(_to)) {
+            IERC223Recipient(_to).tokenReceived(msg.sender, _value, _data);
+        }
+        emit Transfer(msg.sender, _to, _value, _data);
+        return true;
+    }
+    
+    /**
+     * @dev Transfer the specified amount of tokens to the specified address.
+     *      This function works the same with the previous one
+     *      but doesn't contain `_data` param.
+     *      Added due to backwards compatibility reasons.
+     *
+     * @param _to    Receiver address.
+     * @param _value Amount of tokens that will be transferred.
+     */
+    function transfer(address _to, uint _value) public returns (bool success)
+    {
+        bytes memory _empty = hex"00000000";
+        balances[msg.sender] = balances[msg.sender] - _value;
+        balances[_to] = balances[_to] + _value;
+        if(Address.isContract(_to)) {
+            IERC223Recipient(_to).tokenReceived(msg.sender, _value, _empty);
+        }
+        emit Transfer(msg.sender, _to, _value, _empty);
+        return true;
+    }
+}
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract D223ICO {
+
+    address owner = msg.sender;
+
+    uint256 price_rate_BUSDT = 2500 * 1e12; // Target price $0.0004 per D223 token.
+
+    address BUSDT_contract = 0xbf6c50889d3a620eb42C0F188b65aDe90De958c4;
+    address ICO_token;
+
+/*
+    function tokenReceived(address _from, uint _value, bytes memory _data) public override returns (bytes4)
+    {
+        tkn.token_contract = msg.sender;
+        tkn.sender         = _from;
+        tkn.value          = _value;
+        tkn.data           = _data;
+        
+        // ACTUAL CODE
+
+        return 0x8943ec02;
+    }
+*/
+
+    function purchaseTokens(address _payment_token, uint256 _payment_amount) public
+    {
+        if (_payment_token == BUSDT_contract)
+        {
+            IERC20(_payment_token).transferFrom(msg.sender, address(this), _payment_amount);
+            IERC20(ICO_token).transfer(msg.sender, _payment_amount * price_rate_BUSDT);
+        }
+    }
+
+    function getRewardAmount(address _token, uint256 _deposit) public view returns (uint256)
+    {
+        if(_token == BUSDT_contract) return _deposit * price_rate_BUSDT;
+        return 0;
+    }
+
+    function set(uint256 _price_USDT, address _ICO_token, address _BUSDT) public
+    {
+        require(msg.sender == owner);
+        price_rate_BUSDT   = _price_USDT;
+        BUSDT_contract     = _BUSDT;
+        ICO_token          = _ICO_token;
+    }
+
+    function extractTokens(address _token, uint256 _amount) public
+    {
+        require(msg.sender == owner);
+        IERC20(ICO_token).transfer(msg.sender, _amount);
+    }
+}
