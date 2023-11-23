620fa50b75ecde9926b45269df9147f3a21d6e86
Dexaran <dexaran820@gmail.com>
2023-11-24 00:01:09 +0400

    Rounding calculation update

--- contracts.sol
+++ contracts.sol
@@ -229,7 +229,7 @@ contract D223ICO {
 
     address public owner = msg.sender;
 
-    uint256 public price_rate_USD = 1539; // Target price $0.00065 per D223 token.
+    uint256 public price_rate_USD = 1538; // Target price $0.00065 per D223 token.
     uint256 public price_rate_ETH = price_rate_USD * 2000; // Target price $0.0004 per D223 token.
 
     address public USDT_contract  = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
