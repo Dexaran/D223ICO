fe9c5369dca343043894da274ee2d77d4d09cd4d
Dexaran <dexaran820@gmail.com>
2023-12-05 03:05:07 +0400

    Update contracts_v2.sol

--- contracts_v2.sol
+++ contracts_v2.sol
@@ -323,7 +323,7 @@ contract Dex223ICOv2 {
     address public USDT_contract  = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address public USDC_contract  = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public DAI_contract   = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public ICO_token      = 0x7008D42622a8B4eF73e946833EA90E608de9e96B;
+    address public ICO_token      = 0xcCe968120e6Ded56F32fbfe5A2Ec06CBF1e7c8ED;
 
     bool public active = true;
 
